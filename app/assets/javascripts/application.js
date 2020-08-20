629452c1e537eddc7a0bd4bd52229fb53039319a
Simon <acarigua88@mail.com>
2020-08-19 21:42:51 -0400

    second app proyect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,11 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require jquery.min.js
+//= require jquery.dropotron.min.js
+//= require jquery.scrolly.min.js
+//= require jquery.scrollex.min.js
+//= require browser.min.js
+//= require breakpoints.min.js
+//= require util.js
+//= require main.js
